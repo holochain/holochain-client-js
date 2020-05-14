1812c02e55adcca657b823195fff505a7df1abce
Michael Dougherty <maackle.d@gmail.com>
2020-05-13 19:55:19 -0700

    Fix test

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,13 @@
 import test from 'tape'
 import * as msgpack from 'msgpack-lite'
 
-import * as Api from '../src/api/admin'
+import * as Admin from '../src/api/admin'
+import * as Common from '../src/api/common'
 
 test('admin api basic encode/decode', t => {
-  const request: Api.InstallDnaRequest = { path: 'path/to/dna' }
-  const response: Api.InstallDnaResponse = null
-  const [msg, decoder] = Api.request(request)
+  const request: Admin.InstallDnaRequest = { path: 'path/to/dna' }
+  const response: Admin.InstallDnaResponse = null
+  const [msg, decoder] = Common.request(request)
 
   t.deepEqual(msgpack.decode(msg), request)
   t.deepEqual(decoder(msgpack.encode(response)), response)
