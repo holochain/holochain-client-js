206efc49ca38a24b038ebaff6ae8c428bc1c7bc1
Connor Turland <connorturland@gmail.com>
2020-10-16 14:09:51 -0400

    switch to githuub actions

---
+++ install-holochain.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cargo install holochain --git https://github.com/holochain/holochain.git --rev 45dd3f827caea18f41f77486ca2c37149a18b4ca
+cargo install dna_util --git https://github.com/holochain/holochain.git --rev 45dd3f827caea18f41f77486ca2c37149a18b4ca
