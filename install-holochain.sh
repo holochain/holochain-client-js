ae4714e7eda45b930898e545c23ff0cf6566829d
freesig <tomrgowan@gmail.com>
2021-03-03 10:38:21 +1100

    fix install

--- install-holochain.sh
+++ install-holochain.sh
@@ -5,6 +5,6 @@ REV=c251d3017617b543bb1698024a37d7998483570a
 cargo install --force holochain \
   --git https://github.com/holochain/holochain.git \
   --rev $REV
-cargo install --force hc \
+cargo install holochain_cli --force --bin hc \
   --git https://github.com/holochain/holochain.git \
   --rev $REV
