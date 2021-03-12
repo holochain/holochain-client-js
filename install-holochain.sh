3443650710acdf5992d275c7212b1540028f7571
Eric Harris-Braun <eric@harris-braun.com>
2021-03-11 21:37:14 -0500

    Add lair version to install script

--- install-holochain.sh
+++ install-holochain.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 REV=a82372a62d46a503e48f345360d0fb18cc5822d1
+LAIR_REV=a01a40640574d3cfabae33dfe3f861de7bd7a57c
 
 cargo install --force holochain \
   --git https://github.com/holochain/holochain.git \
@@ -8,3 +9,6 @@ cargo install --force holochain \
 cargo install holochain_cli --force --bin hc \
   --git https://github.com/holochain/holochain.git \
   --rev $REV
+cargo install --force lair_keystore \
+      --git https://github.com/holochain/lair.git \
+      --rev $LAIR_REV
