a614af5c4034951d6f3432513aff5e2f78a29d5d
zo-el <joelulahanna@gmail.com>
2021-04-30 21:31:16 +0530

    update rev

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/3de358aa2d1473bd721f4e39228d18c4162096f4.tar.gz";
-  sha256 = "1s15x9yvf6idajyhxhklb6wgmn322n4cy58w2929gw4df8xq4w9s";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/4c54f6ded68963b738373396ba51621d2ddafc41.tar.gz";
+  sha256 = "1qg7fjgy7rqh3wpczhfbs4l45ll13mbddizdac3pznlkd1v0lvvr";
 }) {} }:
 with pkgs;
 
