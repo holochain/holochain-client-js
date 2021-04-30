aee599eca1e0dc9d9a8d376de4bb61ce804c08c7
zo-el <joelulahanna@gmail.com>
2021-04-30 22:00:34 +0530

    pulling in cargo

--- default.nix
+++ default.nix
@@ -4,11 +4,35 @@
 }) {} }:
 with pkgs;
 
-mkShell {
-  buildInputs = [
-    holochain
-    hc
-    lair-keystore
-    python
-  ];
+let
+    #
+    # Make holo-nixpkg's rustPlatform and cargo available here.  This allows us
+    # to make cargo/rustc available to build DNAs in a functionally defined way.
+    # Once we have reliable builds running on Hydra, this will make eg. Github
+    # Actions be able to provision build/test environments quickly...
+    #
+    inherit (rust.packages.stable.rustPlatform) rust;
+    inherit (darwin.apple_sdk.frameworks) CoreServices Security;
+in
+
+{
+  #
+  # An example Node application build procedure; nix-shell's shell.nix will
+  # inherit these buildInputs?  Once we recover dnaPackages and buildDna, we
+  # should be able to build DNAs again in a functionally defined way.
+  #
+  servicelogger = stdenv.mkDerivation rec {
+    name = "servicelogger";
+    src = gitignoreSource ./.;
+
+    buildInputs = [
+      holochain
+      lair-keystore
+      cargo
+    ];
+
+    nativeBuildInputs = [
+      nodejs
+    ];
+  };
 }
