b8ce201c386d596c95109e34a9a8453a82ab1311
zo-el <joelulahanna@gmail.com>
2021-04-30 21:16:38 +0530

    use holo-nixpkgs

--- default.nix
+++ default.nix
@@ -1,21 +1,14 @@
-let
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/9a1f11bacf0a4a8d5cfb83b449df5f726c569c7c.tar.gz";
-    sha256 = "0j4v354rlipifw2ibydr02nv6bwm33vv63197srswkvv6wi6dr9c";
-  };
-  holonix = import (holonixPath) {
-    includeHolochainBinaries = true;
-    holochainVersionId = "custom";
+{ pkgs ? import (fetchTarball {
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/3de358aa2d1473bd721f4e39228d18c4162096f4.tar.gz";
+  sha256 = "1s15x9yvf6idajyhxhklb6wgmn322n4cy58w2929gw4df8xq4w9s";
+}) {} }:
+with pkgs;
 
-    holochainVersion = {
-     rev = "24477159cd80f3a44fd82bba60baa360e76b9f0d";
-     sha256 = "sha256:1qiypsr37v5m1sqbz2mnlwfrnksds88ag8m78fjwszsh6nx1yhgz";
-     cargoSha256 = "0q9nl0wqvyd5jbxq92f1h4l7i439kl5j1bkzxlz929q4m43r3apn";
-     bins = {
-       holochain = "holochain";
-       hc = "hc";
-     };
-    };
-    holochainOtherDepsNames = ["lair-keystore"];
-  };
-in holonix.main
+mkShell {
+  buildInputs = [
+    holochain
+    hc
+    lair-keystore
+    python
+  ];
+}
