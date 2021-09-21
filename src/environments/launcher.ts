9a991d8790801f1d73983ee11fe0bf4ca46dad89
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-21 14:09:15 +0200

    Fixed node fetch issue

--- src/environments/launcher.ts
+++ src/environments/launcher.ts
@@ -1,4 +1,5 @@
 import { InstalledAppId } from "../api/types";
+import fetch from 'cross-fetch';
 
 // This is coupled with https://github.com/holochain/launcher/blob/develop/src-tauri/src/uis/caddy.rs#L13
 export const LAUNCHER_ENV_URL = "/.launcher-env.json";
