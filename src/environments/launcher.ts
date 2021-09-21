84fa7ef153bd34f2274f3b4a4c98609e2505bb27
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-21 14:28:58 +0200

    Better error handling

--- src/environments/launcher.ts
+++ src/environments/launcher.ts
@@ -1,5 +1,5 @@
 import { InstalledAppId } from "../api/types";
-import fetch from 'cross-fetch';
+import fetch from "cross-fetch";
 
 // This is coupled with https://github.com/holochain/launcher/blob/develop/src-tauri/src/uis/caddy.rs#L13
 export const LAUNCHER_ENV_URL = "/.launcher-env.json";
@@ -16,7 +16,18 @@ export async function fetchLauncherEnvironment(): Promise<
   const env = await fetch(LAUNCHER_ENV_URL);
 
   // We are not in the launcher environment
-  if (!env.ok) return undefined;
+  if (!env.ok) {
+    if (env.status === 404) {
+      console.warn(
+        "[@holochain/conductor-api]: you are in a development environment. When this UI is running in the Holochain Launcher, `AppWebsocket.connect()`, `AdminWebsocket.connect()` and `appWebsocket.appInfo()` will have their parameters ignored and substituted by the ones provided by the Holochain Launcher."
+      );
+      return undefined;
+    } else {
+      throw new Error(
+        `Error trying to fetch the launcher environment: ${env.statusText}`
+      );
+    }
+  }
 
   return env.json();
 }
