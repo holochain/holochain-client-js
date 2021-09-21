58b18b8a472c230f57a9c7486b061fca516d1cb2
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-21 14:39:05 +0200

    Better loggign

--- src/environments/launcher.ts
+++ src/environments/launcher.ts
@@ -10,13 +10,26 @@ export interface LauncherEnvironment {
   INSTALLED_APP_ID: InstalledAppId;
 }
 
+// If _launcherEnvironment is undefined, we are in a local development environment
+let _launcherEnvironment: LauncherEnvironment | undefined = undefined;
+
+// Whether we have already fetched the environment, and we don't have to fetch again
+let loaded: boolean = false;
+
 export async function fetchLauncherEnvironment(): Promise<
   LauncherEnvironment | undefined
 > {
+  if (loaded) return _launcherEnvironment;
+
   const env = await fetch(LAUNCHER_ENV_URL);
 
-  // We are not in the launcher environment
-  if (!env.ok) {
+  if (env.ok) {
+    _launcherEnvironment = await env.json();
+    loaded = true;
+    return _launcherEnvironment;
+  } else {
+    loaded = true;
+    // We are not in the launcher environment
     if (env.status === 404) {
       console.warn(
         "[@holochain/conductor-api]: you are in a development environment. When this UI is running in the Holochain Launcher, `AppWebsocket.connect()`, `AdminWebsocket.connect()` and `appWebsocket.appInfo()` will have their parameters ignored and substituted by the ones provided by the Holochain Launcher."
@@ -28,6 +41,6 @@ export async function fetchLauncherEnvironment(): Promise<
       );
     }
   }
-
-  return env.json();
 }
+
+fetchLauncherEnvironment();
