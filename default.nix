6a76a2f12c2bc6a2df26de8900afbe97ca38bdb6
Jost Schulte <jost.schulte@protonmail.com>
2022-08-24 16:00:39 -0600

    chore(nix): update default.nix

--- default.nix
+++ default.nix
@@ -2,13 +2,20 @@ let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
     holochainVersionId = "v0_0_151";
+    include = {
+      holochainBinaries = true;
+      node = false;
+      happs = false;
+      scaffolding = false;
+      niv = false;
+    };
   };
   nixpkgs = holonix.pkgs;
 in
 nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
   packages = with nixpkgs; [
-    niv
+    # niv
     nodejs-16_x
   ];
 }
