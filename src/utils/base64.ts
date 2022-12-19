e045016eccbab858031276cbb6bfb3bc75b420b3
Jost Schulte <jost.schulte@protonmail.com>
2022-12-19 12:09:37 -0600

    use hash encoder in zome call props

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,10 +1,10 @@
 import { Base64 } from "js-base64";
 import { HoloHash, HoloHashB64 } from "../types.js";
 
-export function encodeHashTo64(hash: HoloHashB64): HoloHash {
+export function decodeHashFromBase64(hash: HoloHashB64): HoloHash {
   return Base64.toUint8Array(hash.slice(1));
 }
 
-export function decodeHashFromB64(hash: HoloHash): HoloHashB64 {
+export function encodeHashToBase64(hash: HoloHash): HoloHashB64 {
   return `u${Base64.fromUint8Array(hash, true)}`;
 }
