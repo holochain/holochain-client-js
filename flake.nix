cdedce269865de55bd6aea1fe352c0fc2ed92b42
Jost Schulte <jost.schulte@protonmail.com>
2023-04-14 13:20:58 -0600

    test(fixture): update hdk dep to 0.2

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     holonix.url = "github:holochain/holochain";
-    holonix.inputs.holochain.url = "github:holochain/holochain/9c4f10d16b28c977682010746c4a61641ecb68c8";
+    holonix.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0-beta-rc.4";
     nixpkgs.follows = "holonix/nixpkgs";
   };
 
