1d5065ee9618fbcdb9801daffcb085540d120772
Jost Schulte <jost.schulte@protonmail.com>
2023-08-02 11:36:33 -0600

    feat: export functions to check if role name is clone id

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,5 +3,11 @@ export * from "./admin/index.js";
 export * from "./app-agent/index.js";
 export * from "./app/index.js";
 export { IsoWebSocket, WsClient } from "./client.js";
-export { CloneId, Requester, Transformer } from "./common.js";
+export {
+  CloneId,
+  Requester,
+  Transformer,
+  getBaseRoleNameFromCloneId,
+  isCloneId,
+} from "./common.js";
 export * from "./zome-call-signing.js";
