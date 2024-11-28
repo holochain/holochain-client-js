c4aadbe320e619555be7144daf8f5c99e7f2517f
Matthias <tuttiacc@hotmail.com>
2024-11-28 21:49:38 +0100

    bump hdk verison, fix hc sandbox args and log errors, update flake

--- test/e2e/common.ts
+++ test/e2e/common.ts
@@ -11,20 +11,16 @@ const LAIR_PASSPHRASE = "passphrase";
 
 export const launch = async (port: number) => {
   // create sandbox conductor
-  const args = [
-    "sandbox",
-    "--piped",
-    "create",
-    "--in-process-lair",
-    "--dpki-network-seed",
-    Date.now().toString(),
-  ];
+  const args = ["sandbox", "--piped", "create", "--in-process-lair"];
   const createConductorProcess = spawn("hc", args);
   createConductorProcess.stdin.write(LAIR_PASSPHRASE);
   createConductorProcess.stdin.end();
 
   let conductorDir = "";
   const createConductorPromise = new Promise<void>((resolve) => {
+    createConductorProcess.stderr.on("data", (data) => {
+      console.error("[hc sandbox] ERROR: ", data.toString());
+    });
     createConductorProcess.stdout.on("data", (data) => {
       const tmpDirMatches = data.toString().match(/Created.+"(.+)"/);
       if (tmpDirMatches) {
